be6aeab50f9b0589accaffa47f0eb0678bf932ae
rixo <eric@rixo.fr>
2018-05-30 16:02:45 +0200

    Fix row == 0

--- packages/navigate-refactor-references/lib/util.js
+++ packages/navigate-refactor-references/lib/util.js
@@ -2,13 +2,15 @@
 
 import {Range} from 'atom'
 
+const or = (a, b) => typeof a === 'undefined' ? b : a
+
 export const createLocator = text => {
-	const nl = /\r\n|\n|\r/g
-	const loc = [0]
-	while (nl.exec(text)) {
-		loc.push(nl.lastIndex)
-	}
-	return ({column: col, row, line}) => loc[row || line] + col
+  const nl = /\r\n|\n|\r/g
+  const loc = [0]
+  while (nl.exec(text)) {
+    loc.push(nl.lastIndex)
+  }
+  return ({column: col, row, line}) => loc[or(row, line)] + col
 }
 
 export const locToRange = ({start, end}) =>
